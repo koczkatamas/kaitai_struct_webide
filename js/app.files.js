dcf23af7b69d5ed9a130a16230c8588c3a2fa410
KT <koczkatamas@gmail.com>
2017-04-19 21:17:14 +0200

    Remove every functionality except file download

--- js/app.files.js
+++ js/app.files.js
@@ -121,15 +121,8 @@ $(() => {
     }).bind('loaded.jstree', refreshFsNodes).jstree(true);
     var uiFiles = {
         fileTreeContextMenu: $("#fileTreeContextMenu"),
-        openItem: $('#fileTreeContextMenu .openItem'),
-        createFolder: $('#fileTreeContextMenu .createFolder'),
-        createKsyFile: $('#fileTreeContextMenu .createKsyFile'),
-        cloneKsyFile: $('#fileTreeContextMenu .cloneKsyFile'),
-        generateParser: $('#fileTreeContextMenu .generateParser'),
         downloadItem: $('#fileTreeContextMenu .downloadItem'),
         deleteItem: $('#fileTreeContextMenu .deleteItem'),
-        createLocalKsyFile: $('#createLocalKsyFile'),
-        uploadFile: $('#uploadFile'),
         downloadFile: $('#downloadFile'),
     };
     function convertTreeNode(treeNode) {
@@ -157,11 +150,7 @@ $(() => {
         var isLocal = data && data.fsType === 'local';
         var isKsy = data && data.fn && data.fn.endsWith('.ksy') && !isFolder;
         function setEnabled(item, isEnabled) { item.toggleClass('disabled', !isEnabled); }
-        setEnabled(uiFiles.createFolder, isLocal && isFolder);
-        setEnabled(uiFiles.createKsyFile, isLocal && isFolder);
-        setEnabled(uiFiles.cloneKsyFile, isLocal && isKsy);
         setEnabled(uiFiles.deleteItem, isLocal);
-        setEnabled(uiFiles.generateParser, isKsy);
         uiFiles.fileTreeContextMenu.css({ display: "block", left: e.pageX, top: e.pageY });
         return false;
     });
@@ -173,42 +162,13 @@ $(() => {
             }
         });
     }
-    ctxAction(uiFiles.createFolder, e => {
-        var parentData = getSelectedData();
-        ui.fileTree.create_node(ui.fileTree.get_node(contextMenuTarget), { data: { fsType: parentData.fsType, type: 'folder' }, icon: 'glyphicon glyphicon-folder-open' }, "last", node => {
-            ui.fileTree.activate_node(node, null);
-            setTimeout(function () { ui.fileTree.edit(node); }, 0);
-        });
-    });
     ctxAction(uiFiles.deleteItem, e => ui.fileTree.delete_node(ui.fileTree.get_selected()));
-    ctxAction(uiFiles.openItem, e => $(contextMenuTarget).trigger('dblclick'));
-    ctxAction(uiFiles.generateParser, e => {
-        var fsItem = getSelectedData();
-        var linkData = $(e.target).data();
-        //console.log(fsItem, linkData);
-        fss[fsItem.fsType].get(fsItem.fn).then(content => {
-            return compile(content, linkData.kslang, !!linkData.ksdebug).then(compiled => {
-                compiled.forEach((compItem, i) => {
-                    var title = fsItem.fn.split('/').last() + ' [' + $(e.target).text() + ']' + (compiled.length == 1 ? '' : ` ${i + 1}/${compiled.length}`);
-                    addEditorTab(title, compItem, linkData.acelang);
-                });
-            });
-        });
-    });
     fileTreeCont.on('create_node.jstree rename_node.jstree delete_node.jstree move_node.jstree paste.jstree', saveTree);
     fileTreeCont.on('move_node.jstree', (e, data) => ui.fileTree.open_node(ui.fileTree.get_node(data.parent)));
     fileTreeCont.on('select_node.jstree', (e, selectNodeArgs) => {
         var fsItem = selectNodeArgs.node.data;
         [uiFiles.downloadFile, uiFiles.downloadItem].forEach(i => i.toggleClass('disabled', !(fsItem && fsItem.type === 'file')));
     });
-    var ksyParent;
-    function showKsyModal(parent) {
-        ksyParent = parent;
-        $('#newKsyName').val('');
-        $('#newKsyModal').modal();
-    }
-    ctxAction(uiFiles.createKsyFile, () => showKsyModal(contextMenuTarget));
-    uiFiles.createLocalKsyFile.on('click', () => showKsyModal('localStorage'));
     function downloadFiles() {
         ui.fileTree.get_selected().forEach(nodeId => {
             var fsItem = ui.fileTree.get_node(nodeId).data;
@@ -217,26 +177,6 @@ $(() => {
     }
     ctxAction(uiFiles.downloadItem, () => downloadFiles());
     uiFiles.downloadFile.on('click', () => downloadFiles());
-    uiFiles.uploadFile.on('click', () => openFilesWithDialog(addNewFiles));
-    $('#newKsyModal').on('shown.bs.modal', () => { $('#newKsyModal input').focus(); });
-    $('#newKsyModal form').submit(function (event) {
-        event.preventDefault();
-        $('#newKsyModal').modal('hide');
-        var ksyName = $('#newKsyName').val();
-        var parentData = ui.fileTree.get_node(ksyParent).data;
-        fss[parentData.fsType].put((parentData.fn ? `${parentData.fn}/` : '') + `${ksyName}.ksy`, `meta:\n  id: ${ksyName}\n  file-extension: ${ksyName}\n`).then(fsItem => {
-            addKsyFile(ksyParent, `${ksyName}.ksy`, fsItem);
-            return loadFsItem(fsItem);
-        });
-    });
-    fileTreeCont.bind("dblclick.jstree", function (event) {
-        loadFsItem(ui.fileTree.get_node(event.target).data);
-    });
-    ctxAction(uiFiles.cloneKsyFile, e => {
-        var fsItem = getSelectedData();
-        var newFn = fsItem.fn.replace('.ksy', '_' + new Date().format('Ymd_His') + '.ksy');
-        console.log('newFn', newFn);
-        fss[fsItem.fsType].get(fsItem.fn).then(content => fss[fsItem.fsType].put(newFn, content).then(fsItem => addKsyFile('localStorage', newFn.split('/').last(), fsItem)));
-    });
+    fileTreeCont.bind("dblclick.jstree", function (event) { downloadFiles(); });
 });
 //# sourceMappingURL=app.files.js.map
--- src/app.layout.ts
+++ src/app.layout.ts
@@ -1,69 +1,11 @@
-﻿var practiceChallNameMatch = /practice=([a-z0-9]+)/.exec(location.href);
-var practiceChallName = practiceChallNameMatch ? practiceChallNameMatch[1] : practiceMode.chall;
-var practiceChall = practiceMode && practiceChallName && practiceMode.challs[practiceChallName];
-var isPracticeMode = !!practiceChall;
-
-var myLayout = new GoldenLayout({
+﻿var myLayout = new GoldenLayout({
     settings: { showCloseIcon: false, showPopoutIcon: false },
-    content: [
-        { type: 'row', content: []
-            .concat(isPracticeMode ? [] : { type: 'component', componentName: 'fileTreeCont', title: 'files', isClosable: false, width:12 })
-            .concat(isPracticeMode ? { type: 'component', componentName: 'practicePanel', title: 'practice mode', isClosable: false, width: 25 } : [])
-            .concat(
-                { type: 'column', id: 'mainArea', isClosable: false, content: [
-                    { type: 'row', content: [
-                        { type: 'column', content: [
-                            { type: 'component', componentName: 'ksyEditor', title: '.ksy editor', isClosable: false },
-                            { type: 'stack', activeItemIndex: 0, content: [
-                                //{ type: 'component', componentName: 'parsedDataViewer', title: 'parsed as JSON', isClosable: false },
-                                { type: 'component', componentName: 'parsedDataTree', title: 'parsed as tree', isClosable: false },
-                            ]},
-                        ]},
-                        { type: 'stack', id: 'codeTab', activeItemIndex: 2, content: [
-                            { type: 'component', componentName: 'genCodeViewer', title: 'JS code', isClosable: false },
-                            { type: 'component', componentName: 'genCodeDebugViewer', title: 'JS code (debug)', isClosable: false },
-                            { type: 'column', isClosable: false, title: 'input binary', content: [
-                                { type: 'component', componentName: 'hexViewer', title: 'hex viewer', isClosable: false },
-                                { type: 'row', isClosable: false, height: 35, content: [
-                                    { type: 'component', componentName: 'infoPanel', title: 'info panel', isClosable: false, width: 40 },
-                                    { type: 'component', componentName: 'converterPanel', title: 'converter', isClosable: false },
-                                ]}
-                            ]}
-                        ]}
-                    ]},
-            ]
-            })
-        }
-    ]
+    content: [{ type: 'component', componentName: 'fileTreeCont', title: 'files', isClosable: false, width: 12 }]
 });
 
-function getLayoutNodeById(id) {
-    return (<any>myLayout)._getAllContentItems().filter(x => x.config.id === id)[0];
-}
-
-var dynCompId = 1;
-function addEditorTab(title: string, data: string, lang: string = null, parent: string = 'codeTab') {
-    var componentName = `dynComp${dynCompId++}`;
-    addEditor(componentName, lang, true, editor => editor.setValue(data, -1));
-    getLayoutNodeById(parent).addChild({ type: 'component', componentName, title });
-}
-
 var ui = {
-    ksyEditor: <AceAjax.Editor>null,
-    genCodeViewer: <AceAjax.Editor>null,
-    genCodeDebugViewer: <AceAjax.Editor>null,
-    //parsedDataViewer: <AceAjax.Editor>null,
-    parsedDataTreeCont: <GoldenLayout.Container>null,
-    parsedDataTree: <JSTree>null,
-    hexViewer: <HexViewer>null,
-    errorWindow: <GoldenLayout.Container>null,
-    infoPanel: <GoldenLayout.Container>null,
     fileTreeCont: <JQuery>null,
     fileTree: <JSTree>null,
-    converterPanel: <JQuery>null,
-    practicePanelCont: <GoldenLayout.Container>null,
-    unparsedIntSel: <IntervalViewer>null,
-    bytesIntSel: <IntervalViewer>null,
 };
 
 function addComponent(name: string, generatorCallback?) {
@@ -80,39 +22,5 @@ function addComponent(name: string, generatorCallback?) {
     });
 }
 
-function addExistingDiv(name: string) {
-    myLayout.registerComponent(name, function (container: GoldenLayout.Container, componentState) {
-        ui[name + 'Cont'] = container;
-        ui[name] = $(`#${name}`).appendTo(container.getElement());
-        $(() => ui[name].show());
-    });
-}
-
-function addEditor(name: string, lang: string, isReadOnly: boolean = false, callback = null) {
-    addComponent(name, container => {
-        var editor = ace.edit(container.getElement().get(0));
-        editor.setTheme("ace/theme/monokai");
-        editor.getSession().setMode(`ace/mode/${lang}`);
-        if (lang === 'yaml')
-            editor.setOption('tabSize', 2);
-        editor.$blockScrolling = Infinity; // TODO: remove this line after they fix ACE not to throw warning to the console
-        editor.setReadOnly(isReadOnly);
-        if (callback)
-            callback(editor);
-        return editor;
-    });
-}
-
-addEditor('ksyEditor', 'yaml');
-addEditor('genCodeViewer', 'javascript', true);
-addEditor('genCodeDebugViewer', 'javascript', false);
-//addEditor('parsedDataViewer', 'javascript', true);
-addComponent('hexViewer', () => new HexViewer("hexViewer"));
-addComponent('errorWindow', cont => { cont.getElement().append($("<div />")); });
-addComponent('parsedDataTree');
 addComponent('fileTreeCont', cont => cont.getElement().append($("#fileTreeCont").children()));
-addExistingDiv('infoPanel');
-addExistingDiv('converterPanel');
-addExistingDiv('practicePanel');
-
 myLayout.init();
